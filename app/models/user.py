f315b978af05adf44348bf25fd68741a195ca41a
ongebo <isaacongebo@gmail.com>
2019-08-25 13:16:33 +0300

    Add model to represent users

---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column('username', db.String, unique=True, nullable=False)
+    phone_number = db.Column('phone_number', db.String, unique=True, nullable=False)
+    email = db.Column('email', db.String, unique=True, nullable=False)
+    password = db.Column('password', db.String, nullable=False)
