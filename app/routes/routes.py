c80c048ed9963f0a3373659da313aa376f76d009
ongebo <isaacongebo@gmail.com>
2019-08-26 12:14:12 +0300

    Persist validated user data to database

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,10 +1,13 @@
 from flask import request, jsonify
 from ..utils.validator import app, RequestValidator
+from ..utils.persister import Persister
 
 
 @app.route('/users', methods=['POST'])
 def register_user():
     validator = RequestValidator(request)
     if validator.request_invalid():
-        return jsonify(validator.errors), 400
-    return jsonify({'success': 'Request in JSON Format!'}), 201
+        return jsonify({'errors': validator.errors}), 400
+    persister = Persister(request.get_json())
+    created_data = persister.persist_user()
+    return jsonify(created_data), 201
