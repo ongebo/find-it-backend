c5eb98b731a280cd8fbfec38b9d114f4a48a3a55
ongebo <isaacongebo@gmail.com>
2019-08-26 15:57:11 +0300

    Configure test for request validator

--- tests/utils/conftest.py
+++ tests/utils/conftest.py
@@ -1,6 +1,37 @@
 import os
+import pytest
+
+
+class Request:
+    """A dummy class representing an HTTP request."""
+
+    def __init__(self, body):
+        self.body = body
+
+    def get_json(self):
+        return self.body if isinstance(self.body, dict) else None
 
 
 def pytest_configure(config):
     os.environ['DATABASE_URL'] = os.getenv('TEST_DATABASE')
-    print(os.getenv('DATABASE_URL'))
+
+
+@pytest.fixture
+def redundant_request():
+    return Request({
+        'username': 'John Doe',
+        'phone_number': 'John Doe',
+        'email': 'John Doe',
+        'password': 'JohnDoe',
+        'school': 'not required in request'
+    })
+
+
+@pytest.fixture
+def invalid_fields_request():
+    return Request({
+        'username': '12',
+        'phone_number': '076',
+        'email': 'John Doe',
+        'password': 'JoDoe'
+    })
