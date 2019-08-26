77655569350f87f75c980345ec3a3e010226722b
ongebo <isaacongebo@gmail.com>
2019-08-26 17:53:44 +0300

    Set up test database after changing DATABASE_URL

--- db_setup.py
+++ db_setup.py
@@ -1,9 +1,16 @@
-from app.models.user import db
 import sys
 import os
 
 
 def setup_tables():
     if len(sys.argv) == 2 and sys.argv[1] == 'testdb':
-        os.environ['DATABASE_URL'] = os.getenv['TEST_DATABASE']
+        print('Setting up test database...')
+        os.environ['DATABASE_URL'] = os.getenv('TEST_DATABASE', '')
+
+    # import db only after setting os.environ['DATABASE_URL'] above
+    from app.models.user import db
     db.create_all()
+
+
+if __name__ == "__main__":
+    setup_tables()
