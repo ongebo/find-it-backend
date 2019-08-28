dbd09252535b30a222e530105388cd659299b1d9
ongebo <isaacongebo@gmail.com>
2019-08-28 20:33:18 +0300

    Add endpoints for reporting lost and found items

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,8 +1,12 @@
+import os
 from flask import request, jsonify
 from .. import app
-from ..utils.validator import SignupValidator, LoginValidator
-from ..utils.persister import UserPersister
-from flask_jwt_extended import create_access_token, create_refresh_token
+from ..utils.validator import SignupValidator, LoginValidator, LostAndFoundItemValidator
+from ..utils.persister import UserPersister, LostAndFoundItemPersister
+from flask_jwt_extended import (
+    create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+)
+from werkzeug.utils import secure_filename
 
 
 @app.route('/users', methods=['POST'])
@@ -25,3 +29,30 @@ def login_user():
         'access_token': create_access_token(identity=credentials['email']),
         'refresh_token': create_refresh_token(identity=credentials['email']),
     })
+
+
+@app.route('/items', methods=['POST'])
+@jwt_required
+def report_lost_and_found_item():
+    validator = LostAndFoundItemValidator(request)
+    if validator.request_invalid():
+        return jsonify({'errors': validator.errors}), 400
+    persister = LostAndFoundItemPersister(request.get_json())
+    reported_item = persister.persist_item(reporter_email=get_jwt_identity())
+    return jsonify(reported_item), 201
+
+
+@app.route('/items/images', methods=['POST'])
+@jwt_required
+def post_item_image():
+    validator = LostAndFoundItemValidator(request)
+    if validator.uploaded_image_invalid():
+        return jsonify({'errors': validator.uploaded_image_errors}), 400
+    image_file = request.files['image']
+    secure_name = secure_filename(image_file.filename)
+    image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_name))
+    return jsonify({
+        'image_url': os.path.join(
+            request.base_url, secure_name
+        )
+    }), 200
