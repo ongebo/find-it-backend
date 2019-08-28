3640a4a5c653a9b31197222eceb3b845c31fb9e0
ongebo <isaacongebo@gmail.com>
2019-08-28 20:28:07 +0300

    Add sqlalchemy model for lost and found items

---
+++ app/models/lost_and_found_item.py
@@ -0,0 +1,17 @@
+from .. import db
+from datetime import datetime
+
+
+class LostAndFoundItem(db.Model):
+    __tablename__ = 'lost_and_found_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    image_path = db.Column(db.String)
+    report_date = db.Column(
+        db.DateTime,
+        nullable=False, default=datetime.utcnow()
+    )
+    reporter_id = db.Column('reporter_id', db.ForeignKey('users.id'))
+    reporter = db.relationship('User')
