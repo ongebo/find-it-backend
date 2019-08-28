08d4db549cabee74d8bc4631623b899ac24ff1d0
ongebo <isaacongebo@gmail.com>
2019-08-28 20:31:38 +0300

    Add configuration for lost and found item images

--- app/config.py
+++ app/config.py
@@ -14,3 +14,7 @@ JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 # authentication tokens should expire after 30 days
 JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=30)
 JWT_ACCESS_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=30)
+
+# image upload settings
+UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
+MAX_CONTENT_LENGTH = 6 * 1024 * 1024
