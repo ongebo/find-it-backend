edc160fe141459c227a7f02fd542632dc3b8107e
ongebo <isaacongebo@gmail.com>
2019-08-28 20:30:05 +0300

    Create database persister for lost and found items

--- app/utils/persister.py
+++ app/utils/persister.py
@@ -1,4 +1,4 @@
-from ..models.user import User, db
+from ..models import User, LostAndFoundItem, db
 from werkzeug.security import generate_password_hash
 
 
@@ -36,3 +36,43 @@ class UserPersister:
             'email': created_record.email
         }
         return json_format
+
+
+class LostAndFoundItemPersister:
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+    def persist_item(self, reporter_email):
+        self.strip_spaces()
+        self.reporting_user = User.query.filter_by(
+            email=reporter_email
+        ).first()
+        lost_and_found_item = LostAndFoundItem(
+            name=self.json_data['item_name'],
+            description=self.json_data['description'],
+            image_path=self.json_data['image_url'],
+            reporter_id=self.reporting_user.id
+        )
+        db.session.add(lost_and_found_item)
+        db.session.commit()
+        return self.get_reported_item_as_json()
+
+    def strip_spaces(self):
+        for k, v in self.json_data.items():
+            self.json_data[k] = v.strip()
+
+    def get_reported_item_as_json(self):
+        reported_item = LostAndFoundItem.query.filter_by(
+            name=self.json_data['item_name'],
+            description=self.json_data['description'],
+            image_path=self.json_data['image_url'],
+            reporter_id=self.reporting_user.id
+        ).first()
+        return {
+            'id': reported_item.id,
+            'item_name': reported_item.name,
+            'description': reported_item.description,
+            'image_url': reported_item.image_path,
+            'report_date': reported_item.report_date,
+            'reported_by': self.reporting_user.username
+        }
